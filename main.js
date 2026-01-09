f795d98fe96745ee888bfaa87763a8ab96a795c1
REDoneBP98 <mikel.o.uria@opendeusto.es>
2026-01-09 19:33:55 +0100

    Añadidos algunos comentarios de guia

--- main.js
+++ main.js
@@ -22,7 +22,7 @@ console.log();
 
 const tx5 = new Transaction( nknCoin.crearID() ,myWalletAddress, "048cf5f7394fbd1a76bde65c025d22c8b8e17481b69ec7240bb51ec7285c2c73a5f9e6697ef5be3fd49241646bda0832faa72b57bc79de15de4aaeea0326d53734", 10, "España", "España", "Compra de lavadora");
 tx5.signTransaction(myKey);
-console.log("Transacción creada, aún sin verificar.");
+console.log("Transacción creada, aún sin verificar.");//hay que verificar fondos y transacciones tal y como en la original
 
 console.log("El origen tiene fondos suficientes? ", tx5.saldoSuficiente(myWalletAddress, nknCoin, 10) ? 'Yep' : 'Nop');
 console.log("Origen y destino validos? ", tx5.direccionesValidas(myKey, "819613a98aca1c943b862e578d1b684c4f541121049cc7a31f0249a6e9007656") ? 'Yep' : 'Nop');
@@ -30,11 +30,11 @@ console.log("Origen y destino validos? ", tx5.direccionesValidas(myKey, "819613a
 nknCoin.addTransaction(tx5);
 
 nknCoin.CreacicionDeFondos(myWalletAddress);
-console.log("Recompensa por minar: 0 (pendiente de mirar)");
+console.log("Recompensa por minar: 0 (pendiente de mirar)");//l recompensa es 0 ya que no se pueden minar bloques en EuroX
 console.log("Transacción realizada!");
 
 
-console.log("Balance de mi cuenta: ",nknCoin.getBalanceOfAddress(myWalletAddress));
+console.log("Balance de mi cuenta: ",nknCoin.getBalanceOfAddress(myWalletAddress) + " (-10 por la transacción anterior.)");
 
 //console.log(nknCoin.chain);
 
